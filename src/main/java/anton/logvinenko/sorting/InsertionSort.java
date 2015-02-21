8fa4d3c71c7cd387671417c0e074c0d9adf62920
Anton Logvinenko <anton.logvinenko@gmail.com>
2015-02-21 20:51:14 +0300

    Fixed insertion sort

--- src/main/java/anton/logvinenko/sorting/InsertionSort.java
+++ src/main/java/anton/logvinenko/sorting/InsertionSort.java
@@ -2,10 +2,10 @@ package anton.logvinenko.sorting;
 
 /**
  * Array: [[0] [1 2 3 4 5]]
- *
+ * <p>
  * Take the leftmost element E in the second partition,
  * find its place in the first partition.
- *
+ * <p>
  * Shrink the second partition from the left side, repeat.
  */
 public class InsertionSort extends SortAlgorithm {
@@ -14,10 +14,8 @@ public class InsertionSort extends SortAlgorithm {
 	public void sort(int[] a) {
 		for (int i = 1; i < a.length; i++) {
 
-			for (int j = i; j > 0; j--) {
-				if (a[j - 1] > a[j]) {
-					swap(a, j - 1, j);
-				}
+			for (int j = i; j > 0 && a[j - 1] > a[j]; j--) {
+				swap(a, j - 1, j);
 			}
 		}
 	}
